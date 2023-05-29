f6b471281bb9cd3f849789b2d81e5a970b8fd677
ChangHee Park <arrans9591@gmail.com>
2023-05-29 18:20:16 +0900

    add postgresql 설정

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
 console.log("안녕하세요");
+
+import { DataSource } from "typeorm";
+import { Board } from "./Board.postgres";
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "34.64.124.242",
+  port: 5050,
+  username: "postgres",
+  password: "postgres2022",
+  database: "postgres",
+  synchronize: true,
+  logging: true,
+  entities: [Board],
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("DB접속에 성공했습니다.");
+  })
+  .catch((error) => {
+    console.log("DB접속에 실패했습니다.");
+    console.log(error);
+  });
